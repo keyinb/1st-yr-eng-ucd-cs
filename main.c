cce8f52ef7bf54e949040fa973b7995fe5c31281
Cian Belton <103264027+keyinb@users.noreply.github.com>
2022-12-12 10:25:51 +0000

    C program to find the LCM of 2 numbers

--- main.c
+++ main.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Lab8 Q1 function to get GCD of two numbers
-int getGCD(int val1, int val2)
+
+int getLCM(int val1, int val2)
 {
-    int result = 0;
+    int mult12 = val1 * val2;
+
+    int resultGCD = 0, result = 0;
     for(int i =1; i<=val2; i++)
     {
         if((val1%i)==0 && (val2%i)==0)
         {
-            result = i;
+            resultGCD = i;
         }
     }
+
+    result = mult12 / resultGCD;
     return result;
 }
 
 int main()
 {
-    printf("%d", getGCD(8,12));
+    printf("%d", getLCM(8,12));
 }
